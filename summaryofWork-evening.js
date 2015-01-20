45c8271bc2e73c8010cc7aa7dba5ebef48273914
sdtsui <danielt213@gmail.com>
2015-01-19 21:46:19 -0800

    persistence

---
+++ summaryofWork-evening.js
@@ -0,0 +1,36 @@
+cloudflare
+godaddy
+https://dashboard.heroku.com/apps/secure-wildwood-6905/settings
+secure-wildwood-6905.herokuapps.com
+
+
+https://www.youtube.com/watch?v=5rOiW_xY-kc
+
+5rOiW_xY-kc
+
+
+Images:
+Fullsize
+http://img.youtube.com/vi/<insert-youtube-video-id-here>/0.jpg
+Default thumbnails:
+http://img.youtube.com/vi/<insert-youtube-video-id-here>/1.jpg
+http://img.youtube.com/vi/<insert-youtube-video-id-here>/2.jpg
+http://img.youtube.com/vi/<insert-youtube-video-id-here>/3.jpg
+
+
+http://stackoverflow.com/questions/2068344/how-do-i-get-a-youtube-video-thumbnail-from-the-youtube-api
+
+
+
+nextPageToken, make another request to get the next page
+maxResults = 10;
+JSONOBJ.items = 'array of return elements'
+  items[i].length = 'length of items return'
+  items[i].id.videoID = 'string of particular item'
+    //can use this to find thumbnails as well 
+  items[i].snippet//holds a bunch of texty type stuff
+    .publishedAt = ''
+    .channelID = ''
+    .title = ''
+    .description = ''
+    //also holds thumbnails anyway
